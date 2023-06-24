1f9f514a5a4eb4462a1a26e252f2acbec08fd970
Phantonce <yes201045@gmail.com>
2023-06-24 06:43:23 +0100

    test and jest init

--- config/default.ts
+++ config/default.ts
@@ -3,9 +3,10 @@ export default {
     dbUri: "mongodb+srv://username:password@cluster0.9ahgu.mongodb.net/rest-boiler-api",
     saltWorkFactor: 10,
     accessTokenTtl: "15m",
-    refreshTokenTtl: "1y",
+  refreshTokenTtl: "1y",
     accessTokenPrivateKey: ``,
     accessTokenPublicKey: ``,
     refreshTokenPrivateKey: ``,
-    refreshTokenPublicKey: ``,
+  refreshTokenPublicKey: ``,
+ 
   };
